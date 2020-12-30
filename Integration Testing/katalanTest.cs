fd36ad4b26444cb5cd08f944c221fa01cd2c4733
nabilhossain108 <nabilhossain@iut-dhaka.edu>
2020-12-30 19:46:49 +0600

    done

---
+++ Integration Testing/katalanTest.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace Integration_Testing
+{
+    [TestFixture]
+    public class katalanTest
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new FirefoxDriver();
+            baseURL = "https://www.google.com/";
+            verificationErrors = new StringBuilder();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        [Test]
+        public void TheKatalanTest()
+        {
+            driver.Navigate().GoToUrl("https://www.esheba.cnsbd.com/#/login");
+            driver.FindElement(By.Id("email")).Clear();
+            driver.FindElement(By.Id("email")).SendKeys("hossaintalukder108@gmail.com");
+            driver.FindElement(By.Id("password")).Clear();
+            driver.FindElement(By.Id("password")).SendKeys("4677246&!");
+            driver.FindElement(By.Id("email")).Click();
+            driver.FindElement(By.Id("email")).Click();
+            driver.FindElement(By.XPath("//div/div/div[2]")).Click();
+            driver.FindElement(By.Id("email")).Click();
+            driver.FindElement(By.Id("email")).Click();
+            driver.FindElement(By.XPath("//button[@type='submit']")).Click();
+            // ERROR: Caught exception [ERROR: Unsupported command [doubleClick | id=email | ]]
+            driver.FindElement(By.Id("password")).Click();
+            driver.FindElement(By.XPath("//div/div/div[2]")).Click();
+            driver.FindElement(By.Id("password")).Click();
+            driver.FindElement(By.LinkText("Logout")).Click();
+        }
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
